f8257428ce5b9891fee31b0ccff277e2190ff7af
liuzhao2580 <1457639270@qq.com>
2020-10-23 17:38:11 +0800

    登录页面获取输入数据

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { LoginComponent } from './login/login.component'
 import { NGZORRO_arr } from '@/utils/import-NG-ZORRO'
-import { FormsModule } from '@angular/forms'
+import { FormsModule,ReactiveFormsModule } from '@angular/forms'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NZ_I18N } from 'ng-zorro-antd/i18n'
 import { zh_CN } from 'ng-zorro-antd/i18n'
@@ -23,6 +23,7 @@ registerLocaleData(zh)
         ...NGZORRO_arr,
         AppRoutingModule,
         FormsModule,
+        ReactiveFormsModule,
         HttpClientModule,
         BrowserAnimationsModule,
         IconsProviderModule,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from '@angular/core'
+import {FormBuilder } from '@angular/forms'
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css']
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-    constructor() {}
+    validateForm
+    constructor(private formBuilder: FormBuilder) {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.validateForm = this.formBuilder.group({
+            userName: '',
+            password: ''
+        })
+    }
+
+    // 用户登录
+    submitForm():void{
+        console.log(this.validateForm.value);
+        
+    }
 }
