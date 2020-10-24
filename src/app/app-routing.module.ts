81071df466fb854a4b7adf12867718c37e5c6e41
liuzhao2580 <1457639270@qq.com>
2020-10-24 10:27:56 +0800

    重新修改布局，添加layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 import { LoginComponent } from '@/app/login/login.component'
 const routes: Routes = [
-    { path: '', pathMatch: 'full', redirectTo: '/login' },
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    {
+        path: '',
+        loadChildren: ()=> import('./layout/layout.module').then(m=>m.LayoutModule)
+    }
 ]
 
 @NgModule({
--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DashboardComponent}from './dashboard/dashboard.component'
+import {LayoutComponent} from './layout.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        // component: DashboardComponent
+        loadChildren: () => import('./dashboard/dashboard.component').then(m=>m.DashboardComponent)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
