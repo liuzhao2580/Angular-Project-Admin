422f0b7e57484ddb2666b40c8480ded8c6b0586f
liuzhao2580 <1457639270@qq.com>
2020-10-26 17:34:36 +0800

    删除多余的文件导入

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 import { LoginComponent } from '@/app/login/login.component'
-import { LayoutComponent } from './layout/layout.component'
 import { Error404Component } from './error404/error404.component'
 const routes: Routes = [
     { path: 'login', component: LoginComponent },
