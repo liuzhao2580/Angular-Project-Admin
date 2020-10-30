45eac25661b4bcc408589e7775a2ccffec4e1788
liuzhao2580 <1457639270@qq.com>
2020-10-30 17:34:52 +0800

    添加登录页面的http请求

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core'
+import { DomSanitizer } from '@angular/platform-browser'
 import {FormBuilder, Validators} from '@angular/forms'
 import { Router } from '@angular/router'
+import { HttpClient } from '@angular/common/http'
 
 @Component({
     selector: 'app-login',
@@ -8,18 +10,28 @@ import { Router } from '@angular/router'
     styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+    picCode
     constructor(private formBulider: FormBuilder,
-        private router : Router) {
-
-    }
+        private router : Router,
+        private domSanitizer: DomSanitizer,
+        private httpClient: HttpClient) {}
     validateForm
     ngOnInit() :void {
         this.validateForm = this.formBulider.group({
             userName: ['admin', [Validators.required]],
-            password: ['admin', [Validators.required]]
+            password: ['admin', [Validators.required]],
+            code: ['', [Validators.required]]
         })
+        this.gainPicCode()
     }
 
+    // 首先获取图片验证码
+    gainPicCode():void {
+        this.httpClient.get('/api/pictureCode').subscribe(data=> {
+            console.log(data, 'data')
+             this.picCode = this.domSanitizer.bypassSecurityTrustHtml(data.data)
+        })
+    }
     // 提交按钮
     onSubmit():void {
         for (const key in this.validateForm.controls) {
@@ -29,7 +41,10 @@ export class LoginComponent implements OnInit {
         // 说明校验成功，可以发送数据请求
         if(this.validateForm.status === 'VALID') {
             console.log(this.validateForm.value)
-            this.router.navigateByUrl('/dashboard')
+            // this.router.navigateByUrl('/dashboard')
+            this.httpClient.post('/api/login',this.validateForm.value).subscribe(data=> {
+                console.log(data, 'data')
+            })
         }
     }
 }
