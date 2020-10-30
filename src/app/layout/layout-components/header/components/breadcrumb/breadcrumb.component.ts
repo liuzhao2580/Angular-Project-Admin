63cae74c9e9554533b864f038a6c9e7301665c8c
liuzhao2580 <1457639270@qq.com>
2020-10-30 10:04:23 +0800

    添加面包屑的进场退场动画

--- src/app/layout/layout-components/header/components/breadcrumb/breadcrumb.component.ts
+++ src/app/layout/layout-components/header/components/breadcrumb/breadcrumb.component.ts
@@ -1,12 +1,32 @@
 import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router'
+import { trigger, state, style, animate, transition, keyframes } from '@angular/animations'
 import { filter } from 'rxjs/operators'
+
 import Routes from '@/app/routes'
 
 @Component({
     selector: 'app-breadcrumb',
     templateUrl: './breadcrumb.component.html',
-    styleUrls: ['./breadcrumb.component.scss']
+    styleUrls: ['./breadcrumb.component.scss'],
+    animations: [
+        trigger('breadcrumbAnimate', [
+            transition(':enter', [
+                animate('0.5s', keyframes([
+                    style({ opacity: 0,transform: 'translateX(100px)' }),
+                    style({ opacity: 0.5,transform: 'translateX(50px)' }),
+                    style({ opacity: 1,transform: 'translateX(0px)' })
+                ]))
+            ]),
+            transition(':leave', [
+                animate('0.2s', keyframes([
+                    style({ opacity: 1,transform: 'translateX(0px)' }),
+                    style({ opacity: 0.5,transform: 'translateX(50px)' }),
+                    style({ opacity: 0,transform: 'translateX(100px)' })
+                ]))
+            ])
+        ])
+    ]
 })
 export class BreadcrumbComponent implements OnInit {
     // 面包屑
@@ -19,32 +39,31 @@ export class BreadcrumbComponent implements OnInit {
     }
 
     // 监听路由
-    watchRoute():void {
-        this.router.events.subscribe(event => {
-            if(event instanceof NavigationEnd) {
+    watchRoute(): void {
+        this.router.events.subscribe((event) => {
+            if (event instanceof NavigationEnd) {
                 this.getActivedRouteInfo()
             }
         })
     }
 
     // 获取当前的路由信息
-    getActivedRouteInfo():void {
+    getActivedRouteInfo(): void {
         // 获取当前的路由
-        const getCurrentRoute:string = this.router.url
+        const getCurrentRoute: string = this.router.url
         let gainRoutes = []
-        Routes.forEach(item => {
-            if(getCurrentRoute !== '/dashboard'){
-                if(item.children) {
-                    const getFind = item.children.find(children_item => children_item.path === getCurrentRoute)
-                    if(getFind) {
-                        gainRoutes.push({path: item.path, data: item.data})
-                        gainRoutes.push({...getFind})
+        Routes.forEach((item) => {
+            if (getCurrentRoute !== '/dashboard') {
+                if (item.children) {
+                    const getFind = item.children.find((children_item) => children_item.path === getCurrentRoute)
+                    if (getFind) {
+                        gainRoutes.push({ path: item.path, data: item.data })
+                        gainRoutes.push({ ...getFind })
                     }
-                }
-                else if(item.path === getCurrentRoute) gainRoutes.push({...item})
+                } else if (item.path === getCurrentRoute) gainRoutes.push({ ...item })
             }
         })
         this.breadcrumb = []
-        this.breadcrumb.splice(1, 0, ...gainRoutes) 
+        this.breadcrumb.splice(1, 0, ...gainRoutes)
     }
 }
