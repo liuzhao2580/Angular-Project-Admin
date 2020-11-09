7b096343d85617e78e0b84f9712c3c0a682c8214
liuzhao2580 <1457639270@qq.com>
2020-11-09 13:38:06 +0800

    删除获取后端传递的验证码的功能

--- src/app/api/modules/user.service.ts
+++ src/app/api/modules/user.service.ts
@@ -6,9 +6,9 @@ import { HttpConfigService } from '../http-config.service'
 })
 export class UserService {
     constructor(private http : HttpConfigService) {}
-    // 获取用户登录验证码
+    // 获取 csrf-token 发送一条随机请求
     public login_code_API() {
-        return this.http.httpGET('/pictureCode')
+        return this.http.httpGET('/CSRFToken')
     }
     // 用户登录
     public login_API(params) {
