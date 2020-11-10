b06e8135543eba80f6bdaa7a3af33938517b6ea7
liuzhao2580 <1457639270@qq.com>
2020-11-10 11:06:46 +0800

    导入 NzModalModule 模块

--- src/utils/import-NG-ZORRO.ts
+++ src/utils/import-NG-ZORRO.ts
@@ -3,5 +3,6 @@ import { NzInputModule } from 'ng-zorro-antd/input'
 import { NzButtonModule } from 'ng-zorro-antd/button'
 import { NzLayoutModule } from 'ng-zorro-antd/layout'
 import { NzMessageModule } from 'ng-zorro-antd/message'
+import { NzModalModule } from 'ng-zorro-antd/modal'
 
-export const NGZORRO_arr = [NzLayoutModule, NzInputModule, NzButtonModule, NzFormModule, NzMessageModule]
+export const NGZORRO_arr = [NzLayoutModule, NzInputModule, NzButtonModule, NzFormModule, NzMessageModule, NzModalModule]
