a9af87385911e0744517072626abd2dd48b1b2dc
liuzhao2580 <1457639270@qq.com>
2020-11-10 13:24:08 +0800

    添加token的拦截器，把token拦截放到拦截器最前面

--- src/app/api/http-interceptors/index.ts
+++ src/app/api/http-interceptors/index.ts
@@ -5,14 +5,15 @@ import { TokenInterceptor } from './token-interceptor'
 
 // 注意 multi: true 选项。 这个必须的选项会告诉 Angular HTTP_INTERCEPTORS 是一个多重提供者的令牌，表示它会注入一个多值的数组，而不是单一的值。
 export const httpInterceptorProvider = [
+    
+    {
+        provide: HTTP_INTERCEPTORS,
+        useClass: TokenInterceptor,
+        multi: true
+    },
     {
         provide: HTTP_INTERCEPTORS,
         useClass: BaseInterceptor,
         multi: true
     },
-    {
-        provide: HTTP_INTERCEPTORS,
-        useClass: TokenInterceptor,
-        multi: true
-    }
 ]
--- src/app/api/http-interceptors/token-interceptor.ts
+++ src/app/api/http-interceptors/token-interceptor.ts
@@ -2,13 +2,48 @@
  * token 的拦截器
  */
 import { Injectable } from '@angular/core'
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http'
+import {
+    HttpEvent,
+    HttpInterceptor,
+    HttpHandler,
+    HttpRequest
+} from '@angular/common/http'
+import { Router } from '@angular/router'
+import { NzModalService } from 'ng-zorro-antd/modal'
 
 import { Observable } from 'rxjs'
 
+import { getCookie } from '@/utils/cookies'
+
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(req)
+    constructor(private modal: NzModalService, private router: Router) {}
+    intercept(
+        req: HttpRequest<any>,
+        next: HttpHandler
+    ): Observable<HttpEvent<any>> {
+        const token = getCookie('token')
+        const { url } = req
+        let setHeaders
+        // 说明请求路径中包含 api 需要携带 token 请求头
+        if (url.indexOf('api') !== -1) {
+            // 说明 token 不存在
+            if (!token) {
+                this.router.navigateByUrl('/login').then(() => {
+                    this.modal.warning({
+                        nzTitle: '该用户未登录或登录已失效，请重新登录'
+                    })
+                })
+            } else {
+                setHeaders = {
+                    'Authorization': `Bearer ${token}`
+                }
+            }
+        }
+        const authReq = req.clone({
+            setHeaders
+        })
+        console.log(setHeaders, 'authReq')
+        return next.handle(authReq)
     }
 }
