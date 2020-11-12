03bb07d5ea4a358b5818b75ac8b2c8d02f58543c
liuzhao2580 <1457639270@qq.com>
2020-11-12 13:50:16 +0800

    添加personal组件的头像

--- src/app/layout/layout-components/header/components/personal/personal.component.ts
+++ src/app/layout/layout-components/header/components/personal/personal.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 
 import { getStorage } from '@/utils/storage'
 import { UserService } from '@/app/api/modules/user.service'
+import { UserStoreService } from '@/app/store-service/user-store/user-store.service'
 
 @Component({
     selector: 'app-personal',
@@ -9,7 +10,8 @@ import { UserService } from '@/app/api/modules/user.service'
     styleUrls: ['./personal.component.scss']
 })
 export class PersonalComponent implements OnInit {
-    constructor(private userService: UserService) {}
+    avatarSrc: string
+    constructor(private userService: UserService, private userStore: UserStoreService) {}
 
     ngOnInit(): void {
         this.init_UserInfo()
@@ -21,6 +23,8 @@ export class PersonalComponent implements OnInit {
         this.userService.userInfo_API(userId).subscribe({
             next: ({ data }) => {
                 console.log(data, 'data')
+                this.userStore.setUserInfo(data)
+                this.avatarSrc = data.avatar
             }
         })
     }
