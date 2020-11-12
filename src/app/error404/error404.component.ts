26b338f699b4feb1fd8849b29d704aca9ab814c0
liuzhao2580 <1457639270@qq.com>
2020-11-12 14:36:27 +0800

    404页面使用result模块

--- src/app/error404/error404.component.ts
+++ src/app/error404/error404.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Location } from '@angular/common'
 
 @Component({
-  selector: 'app-error404',
-  templateUrl: './error404.component.html',
-  styleUrls: ['./error404.component.scss']
+    selector: 'app-error404',
+    templateUrl: './error404.component.html',
+    styleUrls: ['./error404.component.scss']
 })
 export class Error404Component implements OnInit {
+    constructor( private location: Location ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
+    // 返回上一页
+    public backHistory():void {
+        this.location.back()
+    }
 }
