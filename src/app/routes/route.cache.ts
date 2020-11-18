6bebe7a3cf4f69ffbbbf0e436a542e57d73ef1e8
liuzhao2580 <1457639270@qq.com>
2020-11-18 13:38:05 +0800

    路由懒加载添加注释

--- src/app/routes/route.cache.ts
+++ src/app/routes/route.cache.ts
@@ -1,17 +1,23 @@
 // https://segmentfault.com/a/1190000011430157
 // https://angular.cn/api/router/RouteReuseStrategy#routereusestrategy
+
+/**
+ * 方法触发的顺序
+ * 确定是否应重用路由 -> 检索以前存储的路线 -> 确定是否应重新连接此路由（及其子树） -> 确定是否应分离此路由（及其子树）以便以后重用 -> 存储分离的路线。 
+ * shouldReuseRoute -> retrieve -> shouldAttach -> shouldDetach -> store
+ */
 import { RouteReuseStrategy, ActivatedRouteSnapshot, DetachedRouteHandle } from '@angular/router'
 export class RouterCache implements RouteReuseStrategy {
     cacheRouteMap: { [key: string]: any } = {}
     // 确定是否应分离此路由（及其子树）以便以后重用
     shouldDetach(route: ActivatedRouteSnapshot): boolean {
-        console.log(4, 'shouldDetach')
+        // console.log(4, 'shouldDetach')
         if (!route.data.keep) return false
         return true
     }
     // 存储分离的路线。
     store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
-        console.log(5, 'store')
+        // console.log(5, 'store')
         this.cacheRouteMap[route.routeConfig.path] = {
             snapshot: route,
             handle
@@ -19,13 +25,13 @@ export class RouterCache implements RouteReuseStrategy {
     }
     // 确定是否应重新连接此路由（及其子树）
     shouldAttach(route: ActivatedRouteSnapshot): boolean {
-        console.log(3, 'shouldAttach')
+        // console.log(3, 'shouldAttach')
         if(route.routeConfig.path === 'login') this.cacheRouteMap = {}
         return !!route.routeConfig && !!this.cacheRouteMap[route.routeConfig.path]
     }
     // 检索以前存储的路线
     retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
-        console.log(2, 'retrieve')
+        // console.log(2, 'retrieve')
         if (
             !route.routeConfig ||
             route.routeConfig.loadChildren ||
@@ -36,7 +42,7 @@ export class RouterCache implements RouteReuseStrategy {
     }
     // 确定是否应重用路由
     shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
-        console.log(1, 'shouldReuseRoute')
+        // console.log(1, 'shouldReuseRoute')
         return future.routeConfig === curr.routeConfig
     }
 }
