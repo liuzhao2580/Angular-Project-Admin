bfcbe55aa991abfbeecf3fd4487307b7a41ee960
liuzhao2580 <1457639270@qq.com>
2020-11-19 16:26:47 +0800

    文档页面添加angular和nz-zorro的连接地址

--- src/app/layout/document/document.component.ts
+++ src/app/layout/document/document.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
+interface documentList {
+    url: string
+    title: string
+    img: string
+}
 @Component({
-  selector: 'app-document',
-  templateUrl: './document.component.html',
-  styleUrls: ['./document.component.scss']
+    selector: 'app-document',
+    templateUrl: './document.component.html',
+    styleUrls: ['./document.component.scss']
 })
 export class DocumentComponent implements OnInit {
+    documentCardList: documentList [] = [
+        {
+            url: 'https://angular.cn/docs',
+            title: 'Angular',
+            img: 'https://angular.cn/assets/images/logos/angular/angular.svg'
+        },
+        {
+            url: 'https://ng-zorro.gitee.io/docs/introduce/zh',
+            title: 'Ant Design of Angular',
+            img: 'https://img.alicdn.com/tfs/TB1g.mWZAL0gK0jSZFtXXXQCXXa-200-200.svg'
+        }
+    ]
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
+    cardClick(url) {
+        window.open(url)
+    }
 }
